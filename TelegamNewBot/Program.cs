cac94580a87790599ab840733916bd6e3b21be54
Sergey Bednyakov <sbednyakov@gmail.com>
2022-06-20 02:50:05 +0300

    Add project files.

---
+++ TelegamNewBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TelegamNewBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
